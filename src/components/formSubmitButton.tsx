eff7d206d35257948bf47d23c8a653c1e64097a6
nidal-intelo <nidal@intelo.ai>
2024-02-28 22:16:30 +0530

    src:component:formSubmitButton: -replaced button with loadingButton

--- src/components/formSubmitButton.tsx
+++ src/components/formSubmitButton.tsx
@@ -4,18 +4,13 @@ import React from "react";
 import { useFormStatus } from "react-dom";
 import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
+import LoadingButton from "./loadingButton";
 
 const FormSubmitButton = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
     const {pending} = useFormStatus();
 
-    console.log("pending: ", pending)
   return (
-    <Button {...props} type='submit' disabled={props.disabled || pending}>
-        <span className=' flex items-center justify-center gap-1'>
-            {pending && <Loader2 size={16} className=' animate-spin'/>}
-            {props.children}
-        </span>
-    </Button>
+    <LoadingButton {...props} type="submit" loading={pending}/>
   )
 }
 
