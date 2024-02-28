9c428e835d41c88eb80a452674e5ff2817082682
nidal-intelo <nidal@intelo.ai>
2024-02-28 22:16:55 +0530

    src:app:jobs:new: -completed new job form

--- src/app/jobs/new/NewJobForm.tsx
+++ src/app/jobs/new/NewJobForm.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import H1 from "@/components/ui/h1";
 import { createJobSchema, createJobValues } from "@/lib/validation";
 import React from "react";
@@ -11,6 +18,10 @@ import Select from "@/components/ui/select";
 import { jobTypes, locationTypes } from "@/lib/job-types";
 import LocationInput from "@/components/locationInput";
 import { X } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import RichTextEditor from "@/components/richTextEditor";
+import { draftToMarkdown } from "markdown-draft-js";
+import LoadingButton from "@/components/loadingButton";
 
 const NewJobForm = () => {
   const form = useForm<createJobValues>({
@@ -46,112 +57,215 @@ const NewJobForm = () => {
           </p>
         </div>
         <Form {...form}>
-          <form className="space-y-4" noValidate onSubmit={handleSubmit(onSubmit)}>
-            <FormField 
-                control={control}
-                name="title"
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>Job Title</FormLabel>
-                        <FormControl>
-                            <Input placeholder="e.g. Frontend Developer" {...field}/>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
+          <form
+            className="space-y-4"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="e.g. Frontend Developer" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
             <FormField
-            control={control}
-            name="type"
-            render={({field}) => (
+              control={control}
+              name="type"
+              render={({ field }) => (
                 <FormItem>
-                    <FormLabel>
-                        Job type
-                    </FormLabel>
-                    <FormControl>
+                  <FormLabel>Job type</FormLabel>
+                  <FormControl>
                     <Select {...field} defaultValue="">
-                        <option value="" hidden>Select an option</option>
-                        {jobTypes.map(jobType => (
-                            <option key={jobType} value={jobType}>{jobType}</option>
-                        ))}
+                      <option value="" hidden>
+                        Select an option
+                      </option>
+                      {jobTypes.map((jobType) => (
+                        <option key={jobType} value={jobType}>
+                          {jobType}
+                        </option>
+                      ))}
                     </Select>
-                    </FormControl>
-                    <FormMessage />
+                  </FormControl>
+                  <FormMessage />
                 </FormItem>
-            )}
-            />
-             <FormField 
-                control={control}
-                name="companyName"
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>Company</FormLabel>
-                        <FormControl>
-                            <Input {...field}/>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
-             <FormField 
-                control={control}
-                name="companyLogo"
-                render={({field: { value, ...fieldValues}}) => (
-                    <FormItem>
-                        <FormLabel>Company logo</FormLabel>
-                        <FormControl>
-                            <Input
-                             {...fieldValues} 
-                             type="file" 
-                             accept="image/*" 
-                             onChange={(e) => {
-                                const file = e.target.files?.[0]
-                                fieldValues.onChange(file)
-                            }}/>
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
+              )}
             />
             <FormField
-            control={control}
-            name="locationType"
-            render={({field}) => (
+              control={control}
+              name="companyName"
+              render={({ field }) => (
                 <FormItem>
-                    <FormLabel>
-                        Location
-                    </FormLabel>
-                    <FormControl>
-                    <Select {...field} defaultValue="">
-                        <option value="" hidden>Select an option</option>
-                        {locationTypes.map(locationType => (
-                            <option key={locationType} value={locationType}>{locationType}</option>
-                        ))}
-                    </Select>
-                    </FormControl>
-                    <FormMessage />
+                  <FormLabel>Company</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
                 </FormItem>
-            )}
+              )}
             />
-            <FormField 
-                control={control}
-                name="location"
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>Office location</FormLabel>
-                        <FormControl>
-                            <LocationInput onLocationSelected={field.onChange} ref={field.ref}/>
-                        </FormControl>
-                        {watch("location") && (
-                            <div className="flex items-center gap-1"> 
-                                <button type="button" onClick={() => {setValue("location","", {shouldValidate: true})}}><X size={20}/></button>
-                                <span className="text-sm">{watch("location")}</span>
-                            </div>
-                        )}
-                        <FormMessage />
+            <FormField
+              control={control}
+              name="companyLogo"
+              render={({ field: { value, ...fieldValues } }) => (
+                <FormItem>
+                  <FormLabel>Company logo</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...fieldValues}
+                      type="file"
+                      accept="image/*"
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        fieldValues.onChange(file);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="locationType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Select {...field} defaultValue="" 
+                    onChange={(e) => {                                 //trigger location field validation when job type is remote
+                        field.onChange(e);
+                        if(e.currentTarget.value === 'Remote') {
+                            trigger("location")
+                        }
+                    }}
+                    >
+                      <option value="" hidden>
+                        Select an option
+                      </option>
+                      {locationTypes.map((locationType) => (
+                        <option key={locationType} value={locationType}>
+                          {locationType}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Office location</FormLabel>
+                  <FormControl>
+                    <LocationInput
+                      onLocationSelected={field.onChange}
+                      ref={field.ref}
+                    />
+                  </FormControl>
+                  {watch("location") && (
+                    <div className="flex items-center gap-1">
+                      <button
+                        type="button"
+                        onClick={() => {
+                          setValue("location", "", { shouldValidate: true });
+                        }}
+                      >
+                        <X size={20} />
+                      </button>
+                      <span className="text-sm">{watch("location")}</span>
+                    </div>
+                  )}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className=" space-y-2">
+              <Label htmlFor="applicationEmail">How to apply</Label>
+              <div className="flex justify-between">
+                <FormField
+                  control={control}
+                  name="applicationEmail"
+                  render={({ field }) => (
+                    <FormItem className="grow">
+                      <FormControl>
+                        <div className="flex items-center">
+                          <Input
+                            id="applicationEmail"
+                            placeholder="Email"
+                            type="email"
+                            {...field}
+                          />
+                          <span className="mx-2">or</span>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
                     </FormItem>
-                )}
+                  )}
+                />
+                <FormField
+                  control={control}
+                  name="applicationUrl"
+                  render={({ field }) => (
+                    <FormItem className="grow">
+                      <FormControl>
+                        <Input
+                          placeholder="Website"
+                          type="url"
+                          {...field}
+                          onChange={(e) => {
+                            field.onChange(e);
+                            trigger("applicationEmail");
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <FormField
+              control={control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <Label
+                  onClick={() => setFocus("description")}
+                  >Description</Label>
+                  <FormControl>
+                    <RichTextEditor
+                      onChange={(draft) =>field.onChange(draftToMarkdown(draft))}
+                      ref={field.ref}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
+             <FormField
+              control={control}
+              name="salary"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Salary</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="number"/>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <LoadingButton type="submit" loading={isSubmitting}>Submit</LoadingButton>
           </form>
         </Form>
       </div>
