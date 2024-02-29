d653217f38b02e333218d637f5a7efa8ab5e0229
nidal-intelo <nidal@intelo.ai>
2024-03-01 00:50:21 +0530

    src:components:markdown: added new component tp handle markdown templates.

---
+++ src/components/markdown.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactMarkdown from 'react-markdown'
+
+interface MarkdownProps {
+    children: string
+}
+export default function Markdown({children}: MarkdownProps) {
+  return (
+    <ReactMarkdown
+    className='space-y-3'
+    components={{
+        ul:(props) => <ul className='list-inside list-disc' {...props}/>,
+        a: (props) => <a className='text-violet-500 underline' target='_blank' {...props}/>
+    }}
+    >{children}
+    </ReactMarkdown>
+  )
+}
