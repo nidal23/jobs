af97fc0d32b977e84bc4ae6bc5250254d49ad0e4
nidal-intelo <nidal@intelo.ai>
2024-02-29 22:32:12 +0530

    src:app:job-submitted: -added new page where the user will be redirected to afeter successfull job submission.

---
+++ src/app/job-submitted/page.tsx
@@ -0,0 +1,13 @@
+import H1 from '@/components/ui/h1'
+import React from 'react'
+
+const JobSubimttedPage = () => {
+  return (
+    <main className=' m-auto max-w-5xl space-y-5 text-center'>
+        <H1>Job Submitted</H1>
+        <p>Your job posting has been submitted and is pending approvel.</p>
+    </main>
+  )
+}
+
+export default JobSubimttedPage
