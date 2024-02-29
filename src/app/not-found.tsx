c48413d37382543edc743a5916dd8f106a75466b
nidal-intelo <nidal@intelo.ai>
2024-03-01 00:49:52 +0530

    src:app:not-found: -added a not found page.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import H1 from '@/components/ui/h1'
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <main className=' max-w-5xl m-auto my-10 space-y-5 px-3 text-center'>
+        <H1>Not Found</H1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+    </main>
+  )
+}
+
+export default NotFoundPage
